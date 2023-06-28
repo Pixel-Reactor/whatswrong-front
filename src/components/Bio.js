b7b6dfddd99d54c74b98bbb5a655c522e4dc545f
nico-home-windows <caramesgarcianico@gmail.com>
2023-06-28 11:14:31 +0200

    sistema de likes & colaboraciones en bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { GetUser, ModifyUser } from "../Api/Api";
 import { useUser } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
+import { Colaboraciones } from "./Colaboraciones";
 
 const Bio = () => {
   const navigate = useNavigate();
@@ -12,24 +13,32 @@ const Bio = () => {
   const [username, setUsername] = useState();
   const [bioText, setBioText] = useState();
   const [file, setFile] = useState();
+  const [avatar, setAvatar] = useState(
+    `http://localhost:4000/files/default_avatar.png`
+  );
 
   useEffect(() => {
     const getUser = async () => {
       // console.log(user);
 
       try {
-        const res = await GetUser(user.token);
-        // console.log(res);
-        // console.log(bio.avatar)
+        const res = await GetUser(user?.token);
 
-        if (res.statusText === "OK") {
-          setBio(res.data.data[0]);
+        if (res?.statusText === "OK") {
+          setBio(res?.data.data[0]);
+          // console.log(res);
+          // console.log(JSON.parse(res?.data.data[0].avatar).name);
+          setAvatar(
+            `http://localhost:4000/files/${
+              JSON.parse(res?.data.data[0].avatar).name
+            }`
+          );
         }
-        // console.log(bio);
       } catch (error) {
         console.log(error);
       }
     };
+
     getUser();
   }, []);
 
@@ -52,59 +61,63 @@ const Bio = () => {
 
   return (
     <section className="bio_section">
-      {/* <p>{bio.response.data?.message}</p> */}
-      {!mod && (
-        <article className="bio_det_box">
-          <div>
-            <img
-              src={`http://localhost:4000/files/avatar/${bio.avatar}`}
-              alt="avatar"
-            />
-          </div>
-          <div className="bio_det flex-column-evenly">
-            <h2>{bio.nombre}</h2>
-            <p>{bio.username}</p>
+      <article>
+        {/* <p>{bio.response.data?.message}</p> */}
+        {!mod && (
+          <article className="bio_det_box">
+            <div>
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="bio_det flex-column-evenly">
+              <h2>{bio.nombre}</h2>
+              <p>{bio.username}</p>
 
-            <p>{bio.biografia}</p>
-          </div>
-        </article>
-      )}
-      {mod && (
-        <form onSubmit={handleSubmit} className="bio_form flex-column-evenly">
-          <input
-            type="text"
-            name="nombre"
-            id="nombre"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
-            placeholder="Nombre"
-          />
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-          />
-          <textarea
-            name="bio"
-            id="bio"
-            value={bioText}
-            onChange={(e) => setBioText(e.target.value)}
-            placeholder="Biografia..."
-          />
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-          <button type="submit">Enviar</button>
-        </form>
-      )}
-      {/* FALTA IMPLEMENTAR DES LOGUEO PORQUE CAMBIA DATOS Y EL TOKEN YA NO VALE */}
-      <button onClick={() => setMod(!mod)}>
-        {mod ? "Cancelar" : "Modifiar usuario"}
-      </button>
-      <button onClick={() => setMod(!mod)}>
-        {mod ? "" : "Modifiar password"}
-      </button>
+              <p>{bio.biografia}</p>
+            </div>
+          </article>
+        )}
+        {mod && (
+          <form onSubmit={handleSubmit} className="bio_form flex-column-evenly">
+            <input
+              type="text"
+              name="nombre"
+              id="nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              placeholder="Nombre"
+            />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+            <textarea
+              name="bio"
+              id="bio"
+              value={bioText}
+              onChange={(e) => setBioText(e.target.value)}
+              placeholder="Biografia..."
+            />
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+            <button type="submit">Enviar</button>
+          </form>
+        )}
+        {/* FALTA IMPLEMENTAR DES LOGUEO PORQUE CAMBIA DATOS Y EL TOKEN YA NO VALE */}
+        <button onClick={() => setMod(!mod)}>
+          {mod ? "Cancelar" : "Modifiar usuario"}
+        </button>
+        <button onClick={() => setMod(!mod)}>
+          {mod ? "" : "Modifiar password"}
+        </button>
+      </article>
+      <article>
+        <ul>
+          <Colaboraciones />
+        </ul>
+      </article>
     </section>
   );
 };
--- src/components/Service.js
+++ src/components/Service.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { GetService, SendComment } from "../Api/Api";
+import { AddLike, GetLikesServices, GetService, SendComment } from "../Api/Api";
 import { useNavigate, useParams } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import { ReactComponent as Corazon } from "../images/corazon.svg";
+import { Corazon1 } from "../hooks/useHandleCorazon";
 
 const Service = () => {
+  const { user } = useUser();
   const [servicedet, setServicedet] = useState();
   const [owner, setOwner] = useState();
   const [coments, setComents] = useState();
   const { id } = useParams();
   const [comentarioText, setComentarioText] = useState();
   const navigate = useNavigate();
-  const { user } = useUser();
+  const [likePulsado, setLikePulsado] = useState("");
+  const [numLikesServices, setNumLikesServices] = useState();
 
   const Fecha = (fecha) => {
     const date = new Date(fecha);
@@ -23,22 +27,30 @@ const Service = () => {
       return `hoy`;
     }
   };
-    
+
   useEffect(() => {
     const service = async () => {
-      //test de conexion con la base de datos
       try {
         const response = await GetService(id);
-
-        console.log(response.data.dataService[0].fichero);
+        const res = await GetLikesServices(id);
+        // console.log(response.data.dataService[0].fichero);
         // console.log(response.data.dataService);
-        // console.log(response.data.dataComents); 
+        // console.log(response.data.dataComents);
+        // console.log(res.data.data);
+        setNumLikesServices(res.data.data.length);
+        // console.log(user);
+        const encontrar = res.data.data.find((e) => e.users_id === user.id);
+        // console.log(encontrar);
+
+        if (encontrar) {
+          setLikePulsado(encontrar.idlikes);
+        }
 
         if (response.statusText === "OK") {
           setServicedet(response.data.dataService[0]);
-          console.log(servicedet)
+          // console.log(servicedet)
           setOwner(response.data.owner[0]);
-          console.log(owner)
+          // console.log(owner)
           setComents(response.data.dataComents);
         }
       } catch (error) {
@@ -46,7 +58,7 @@ const Service = () => {
       }
     };
     service();
-  }, [comentarioText]);
+  }, [comentarioText, likePulsado]);
 
   // console.log(oneService);
   // console.log(coments);
@@ -71,59 +83,93 @@ const Service = () => {
     setComentarioText(e.target.value);
   };
 
+  const handleLike = async (e) => {
+    // const corazonDiv = e.target.parentElement;
+    try {
+      if (likePulsado > 0) {
+        await AddLike(
+          {
+            servicios_id: servicedet.idservicios,
+            idLikes: likePulsado,
+          },
+          user.token
+        );
+        setLikePulsado("");
+      } else {
+        const res = await AddLike(
+          {
+            servicios_id: servicedet.idservicios,
+          },
+          user.token
+        );
+        setLikePulsado(res.data.insertId);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="services flex-column-center-top">
-      
       <div className="flex-column-center-top service_box">
-      <div className="flex-center-center">
-            
-              {owner?.avatar ? (
-              <img
-                className="bio_img"
-                src={`http://localhost:4000/img/link/${owner.avatar}`}
-                alt="avatar"
-                width={"20px"}
-              />
-            ) : (
-              <img className="bio_img"
-                src={require('../images/default_avatar.png')}
-                alt="avatar"
-                width={"20px"}
-              />
-            )}
-              <p>
-               
-                <b className="margin-5">{owner?.username ?? ''}</b>
-              </p>
-              <p className="margin-5">pregunta : </p>{" "}
-            </div>
+        <div className="flex-center-center">
+          {owner?.avatar ? (
+            <img
+              className="bio_img"
+              src={`http://localhost:4000/img/link/${owner.avatar}`}
+              alt="avatar"
+              width={"20px"}
+            />
+          ) : (
+            <img
+              className="bio_img"
+              src={require("../images/default_avatar.png")}
+              alt="avatar"
+              width={"20px"}
+            />
+          )}
+          <p>
+            <b className="margin-5">{owner?.username ?? ""}</b>
+          </p>
+          <p className="margin-5">pregunta : </p>{" "}
+        </div>
         <div className="service_card_det flex-center-right">
           <p className="button-small-green flex-center-center">
             {servicedet?.finalizado ? "cerrado" : "abierto"}{" "}
           </p>
         </div>
         <div className="margin-5">
-          <h2 className="card_title">{servicedet?.titulo ?? 'loading'}</h2>
+          <h2 className="card_title">{servicedet?.titulo ?? "loading"}</h2>
         </div>
         <div className="margin-y-10-x-5">
-          <p>{servicedet?.descripcion ?? 'loading'}</p>
+          <p>{servicedet?.descripcion ?? "loading"}</p>
         </div>
-        {servicedet?.fichero? 
-         <div className="question_img_box">
-          <img src={`http://localhost:4000/img/link/${JSON.parse(servicedet.fichero).name}`} width={'100%'} alt="" />
-          </div> : ' '}
-        
-         
+        {servicedet?.fichero ? (
+          <div className="question_img_box">
+            <img
+              src={`http://localhost:4000/img/link/${
+                JSON.parse(servicedet.fichero).name
+              }`}
+              width={"100%"}
+              alt=""
+            />
+          </div>
+        ) : (
+          " "
+        )}
+        <div className="boton_like" onClick={handleLike}>
+          <Corazon className={likePulsado > 0 ? "rojo" : ""} />
+          <span>{numLikesServices} likes</span>
+        </div>
         <div className="service_card_owner flex-column-left">
           <div className="flex-center-between">
-
-          
             <div>
-              <p> {servicedet?.create_at? Fecha(servicedet.create_at) : ''}</p>
+              <p> {servicedet?.create_at ? Fecha(servicedet.create_at) : ""}</p>
             </div>
           </div>
-        </div></div>
-        {user.token ? (
+        </div>
+      </div>
+      {user.token ? (
         <form onSubmit={handleSubmit} className="service_form">
           <textarea
             name="comentario"
@@ -145,23 +191,23 @@ const Service = () => {
       <ul className="services_ul flex-column-center">
         {coments
           ?.sort((a, b) => b.idcomentarios - a.idcomentarios)
-          .map((e) => (
-            <li key={e.idcomentarios} className="services_li">
-              <p>{e.comentario}</p>
+          .map((el) => (
+            <li key={el.idcomentarios} className="services_li">
+              <p>{el.comentario}</p>
               <span>
                 {" "}
-                {`Autor: ${e.users_id} ${new Date(
-                  e.create_at
+                {`Autor: ${el.users_id} ${new Date(
+                  el.create_at
                 ).toLocaleDateString()} `}
               </span>
+              <div>
+                <Corazon1 comentarios_id={el.idcomentarios} />
+              </div>
             </li>
           )) ?? "Servicio no encontrado"}
       </ul>
     </div>
   );
 };
-   
-
-
 
 export default Service;
