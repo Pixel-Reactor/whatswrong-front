351f0691a1317bcb58ef9088ffe1a4c7c8283a58
Matteo <matteo.stella.web@gmail.com>
2023-07-02 21:17:30 +0200

    links restored

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -35,7 +35,7 @@ export const UserProvider = ({ children }) => {
   const imgname = JSON.parse(img);
   if(imgname.name){
     return  <img
-    src={`http://192.168.1.143:4000/img/link/${imgname.name}`}
+    src={`http://localhost:4000/img/link/${imgname.name}`}
     alt="avatar"
   />
   }else{
@@ -64,12 +64,12 @@ export const UserProvider = ({ children }) => {
     try {
       const filename = JSON.parse(file);
       if (filename.type.indexOf('image') > -1) {
-        return <img src={`http://192.168.1.143:4000/img/link/${filename.name}` } width={'100%'} />
+        return <img src={`http://localhost:4000/img/link/${filename.name}` } width={'100%'} />
       } else {
         if (filename.type.indexOf('pdf') > -1){
 
-         return <object  width="100%" height="100%" type="application/pdf" data={`http://192.168.1.143:4000/img/link/${filename.name}?#zoom=85&scrollbar=0&toolbar=0&navpanes=0`}>
-           <embed src={`http://192.168.1.143:4000/img/link/${filename.name}`} type="application/pdf"></embed>
+         return <object  width="100%" height="100%" type="application/pdf" data={`http://localhost:4000/img/link/${filename.name}?#zoom=85&scrollbar=0&toolbar=0&navpanes=0`}>
+           <embed src={`http://localhost:4000/img/link/${filename.name}`} type="application/pdf"></embed>
            <div  className="width-100 flex-center-center padding-10"><p className="button-4 ">Este browser no suporta pdf, <br />descargarlo con el boton de arriba</p></div>
            </object>
 
